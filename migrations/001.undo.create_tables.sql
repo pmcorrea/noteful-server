50b4acb1f25cce76af63c9ee9e0d99f4d6152d6c
pmcorrea <pmcorrea@me.com>
2020-04-22 20:06:50 -0400

    Minor refactoring.

--- migrations/001.undo.create_tables.sql
+++ migrations/001.undo.create_tables.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS folders;
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS connections;
+DROP TABLE IF EXISTS users;
 DROP TYPE user_status;
 DROP TYPE visibility;
 DROP TYPE blocked_status;
